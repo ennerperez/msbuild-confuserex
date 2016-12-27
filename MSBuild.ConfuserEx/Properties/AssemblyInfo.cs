4cb523e69e5eb1bf93916d83e3020a0f56a99e07
Enner Pérez <ennerperez@gmail.com>
2016-12-27 11:53:59 -0400

    Initial develop commit

---
+++ MSBuild.ConfuserEx/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+[assembly: AssemblyTitle("MSBuild.ConfuserEx")]
+[assembly: AssemblyDescription("MSBuild targets for ConfuserEx")]
+#if DEBUG
+[assembly: AssemblyConfiguration("DEBUG")]
+#else
+[assembly: AssemblyConfiguration("RELEASE")]
+#endif
+[assembly: AssemblyCompany("yck1509")]
+[assembly: AssemblyProduct("MSBuild.ConfuserEx")]
+[assembly: AssemblyCopyright("Copyright (C) Ki 2014")]
+
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
